e6d81257a5ae1eb1c858c6b2d06d5e2f235f822d
Omer <110389255+OmerAvi96@users.noreply.github.com>
2023-04-03 16:18:11 +0300

    Add files via upload

--- test/Array.spec.js
+++ test/Array.spec.js
@@ -1,5 +1,8 @@
-import { describe, it } from 'mocha';
-import { assert } from 'chai';
+// import { describe, it } from 'mocha';
+// import { assert } from 'chai';
+
+const { describe, it } = require('mocha');
+const { assert } = require('chai');
 
 describe('Basic Array Test', () => {
     it('should return -1 when the value is not present', () => {
