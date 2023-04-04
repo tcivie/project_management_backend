b8904c306a9fd2e980d9af3582b97757dae4846e
Gleb Tcivie <gleb@tcivie.com>
2023-04-04 22:49:06 +0300

    Fixed the tests

--- test/UserDBTests.spec.js
+++ test/UserDBTests.spec.js
@@ -11,35 +11,43 @@ mongoose
     .connect(process.env.DBURL)
     .then(() => {
         console.log('Connected to DB');
+
+        describe('User not found test', async () => {
+            it('should return a null object when not found', async () => {
+                // eslint-disable-next-line no-undef
+                const user = await User.findOne({ username: '' });
+                assert.equal(null, user);
+            });
+        });
+
+        describe('User found test', async () => {
+            it('should return a user object when found', async () => {
+                // eslint-disable-next-line no-undef
+                const user = await User.findOne({ username: 'omerA' });
+                const found = user !== null;
+                assert.equal(true, found);
+            });
+        });
+
+        describe('Validate User password test', async () => {
+            it('should return false for the wrong password and true for the correct password', async () => {
+                // eslint-disable-next-line no-undef
+                const user = await User.findOne({ username: 'omerA' });
+                const wrongPass = '123456';
+                const correctPass = '12345';
+                assert.equal(false, await user.validPassword(wrongPass));
+                assert.equal(true, await user.validPassword(correctPass));
+            });
+        });
+        mongoose.connection.close();
+        process.exit();
     })
     .catch((error) => {
         console.log(error);
     });
 
-describe('User not found test', async () => {
-    it('should return a null object when not found', async () => {
-        // eslint-disable-next-line no-undef
-        const user = await User.findOne({ username: '' });
-        assert.equal(null, user);
-    });
-});
 
-describe('User found test', async () => {
-    it('should return a user object when found', async () => {
-        // eslint-disable-next-line no-undef
-        const user = await User.findOne({ username: 'omerA' });
-        const found = user !== null;
-        assert.equal(true, found);
-    });
-});
 
-describe('Validate User password test', async () => {
-    it('should return false for the wrong password and true for the correct password', async () => {
-        // eslint-disable-next-line no-undef
-        const user = await User.findOne({ username: 'omerA' });
-        const wrongPass = '123456';
-        const correctPass = '12345';
-        assert.equal(false, await user.validPassword(wrongPass));
-        assert.equal(true, await user.validPassword(correctPass));
-    });
-});
+
+
+
