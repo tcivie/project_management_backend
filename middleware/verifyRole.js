4c92e233a37435757a138c57aff1fb4d7847a700
Gleb Tcivie <gleb@tcivie.com>
2023-05-15 21:54:06 +0300

    Added delete tests for users

---
+++ middleware/verifyRole.js
@@ -0,0 +1,10 @@
+const roleCheck = (roles) => {
+    return (req, res, next) => {
+        if (!req.roles || !roles.some(role => req.roles.includes(role))) {
+            return res.status(403).json({ message: 'Forbidden: Insufficient permissions' });
+        }
+        next();
+    }
+}
+
+module.exports = roleCheck;
