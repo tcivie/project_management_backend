ec29613a280147ce4fcd23f8b377414d1162f227
Omer <110389255+OmerAvi96@users.noreply.github.com>
2023-05-15 13:49:22 +0300

    Added actions checks

--- routers/apiRoutes.js
+++ routers/apiRoutes.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-router.use('/auth', require('./authRoutes'))
-router.use('/users', require('./userRoutes'))
+router.use('/auth', require('./authRoutes'));
+router.use('/users', require('./userRoutes'));
 
-module.exports = router
+module.exports = router;
--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -1,15 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/authController')
-const loginLimiter = require('../middleware/loginLimiter')
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const loginLimiter = require('../middleware/loginLimiter');
 
-router.route('/')
-    .post(loginLimiter, authController.login)
+router.route('/').post(loginLimiter, authController.login);
 
-router.route('/refresh')
-    .get(authController.refresh)
+router.route('/refresh').get(authController.refresh);
 
-router.route('/logout')
-    .post(authController.logout)
+router.route('/logout').post(authController.logout);
 
-module.exports = router
+module.exports = router;
