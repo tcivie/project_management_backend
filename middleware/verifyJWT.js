0e9467b77396e5c85ca35866c6255aeaa95890eb
Gleb Tcivie <gleb@tcivie.com>
2023-05-22 15:25:45 +0300

    Modified backend to work with the new Drawer in the Front

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken');
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if (!authHeader?.startsWith('Bearer ')) {
-        // return res.status(401).json({ message: 'Unauthorized' });
         req.user = null;
         req.roles = 0;
         req.active = false;
