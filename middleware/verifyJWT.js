0e71d1d28a524a15f32d1ff3b194dff3fc1bdd57
Gleb Tcivie <gleb@tcivie.com>
2023-06-10 17:58:27 +0300

    Added support for image upload/update and post update

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const verifyJWT = (req, res, next) => {
+    // console.log('Verifying JWT');
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if (!authHeader?.startsWith('Bearer ')) {
         req.id = null;
--- routers/chatRoutes.js
+++ routers/chatRoutes.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 const chatController = require('../controllers/chatController');
-const { uploadMultiple } = require('../utils/imageUploader');
+const { uploadMultiple, deleteExistingImages } = require('../utils/imageUploader');
 const verifyJWT = require('../middleware/verifyJWT');
 const checkActive = require('../middleware/checkActivity');
+const { CanPerfomAction } = require('../middleware/checkRoles');
 
 router.route('/languages').get(chatController.getLanguages);
 router.route('/posts/post/:postId/users').get(chatController.getUsersInChat);
 router.route('/posts/post/:postId').get(chatController.getChatHistory);
+router.route('/posts/post/getSinglePost/:postId').get(chatController.getPost);
 router.use(verifyJWT);
 router.route('/posts/city/:cityId/:language').get(chatController.getPostsByLanguage);
 router.route('/posts/city/:cityId').get(chatController.getPosts);
@@ -17,5 +19,8 @@ router.route('/posts/post/setHelpful').post(chatController.setHelpful);
 router.route('/posts/post/unsetHelpful').post(chatController.unsetHelpful);
 router.route('/posts/post/:postId').post(chatController.sendMessage);
 router.route('/posts').post(uploadMultiple, chatController.createPost);
+router.use(CanPerfomAction());
+router.route('/posts/delete/:postId').delete(chatController.deletePost);
+router.route('/posts/update/:postId').patch(uploadMultiple, deleteExistingImages, chatController.updatePost);
 
 module.exports = router;
